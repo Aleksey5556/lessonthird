6a27edf1240e71b7503b0806e31a090c863d82e8
Aleksey5556 <96414248+Aleksey5556@users.noreply.github.com>
2022-04-27 14:12:15 +0300

    Initial commit

---
+++ src/Сollection/Sheet.java
@@ -0,0 +1,29 @@
+package Сollection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Sheet {
+    public static void main(String[] args) {
+        String[] words = {"Дом","Дом","Апильсин","Апильсин","Апильсин","Ручка"
+                ,"Мышка","Мышка","Ручка","Ракета","Река",
+                "Ракета","Гриб","Зайц","Зайц","Гриб" };
+
+        HashSet<String> set =new HashSet<>();
+        for (String e :words){
+          set.add(e);
+        }
+        for (String e: set) {
+            int quantity = 0;
+            for (int i = 0; i < words.length; i++){
+                if (e.equals(words[i])){
+                    quantity++;
+                }
+            }
+            System.out.println(e +" " + quantity);
+        }
+
+        System.out.println(set);
+    }
+
+}
