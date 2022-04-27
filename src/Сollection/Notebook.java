690f128b6bfbc0c3154c07f3e01398dc8e043309
Aleksey5556 <96414248+Aleksey5556@users.noreply.github.com>
2022-04-27 14:12:15 +0300

    Initial commit

---
+++ src/Сollection/Notebook.java
@@ -0,0 +1,29 @@
+package Сollection;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Notebook {
+
+
+    private HashMap <String, HashSet<String>> map;
+
+
+    public Notebook(){
+        this.map = new HashMap<>();
+    }
+    public void add(String surname,String telephone){
+        if (!map.containsKey(surname)){
+            map.put(surname,new HashSet<>());
+        }
+        map.get(surname).add(telephone);
+    }
+
+    public HashSet<String> find(String surname) {
+if (!map.containsKey(surname)){
+    return new  HashSet<>();
+}
+return map.get(surname);
+    }
+
+}
